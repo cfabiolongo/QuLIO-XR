6d0d2db768561efa4b7bbb064c7ffc07f46c0edf
fabio <fabiuslongo@gmail.com>
2024-05-17 16:10:58 +0200

    Building explorative query sparql builder

--- logform_builder.py
+++ logform_builder.py
@@ -4,7 +4,7 @@ from actions import *
 
 # Logical form BUILDER from LODO KGs
 
-# +Q("Colonel_NNP_West_NNP"), +Q("Cat_NN"), +FEED("the cat ate the food voraciously")
+# +Q("Colonel_NNP_West_NNP"), +Q("Nono_NNP"), +Q("Missile_NNS")
 
 #proc_logform(X) >> [show_line("\nLooking for all memberships of ", X), log("SPARQL: ", X), submit_explo_membership(X), proc_logform()]
 proc_logform(X) >> [show_line("\nLaunching explorative query SPARQL: ", X), log("SPARQL: ", X), submit_explo_sparql(X), submit_intr_explo_sparql(X), proc_logform()]
@@ -14,7 +14,7 @@ proc_logform(X) >> [show_line("\nLaunching explorative query SPARQL: ", X), log(
 # seek_prep(V), seek_adj(S), seek_prep(S), seek_adj(O), seek_prep(O)
 
 #  seek_adj(S), seek_adj(O), seek_prep(S), seek_prep(V), seek_adv(V)
-proc_logform() / VF(I, V, S, O) >> [show_line(f"\nProc. ",I,"..."), -VF(I, V, S, O), +LF_ORIGIN(I, V, S, O), build_pre(I, V, S, O), proc_logform()]
+proc_logform() / VF(I, V, S, O) >> [show_line(f"\nProc. ",I,"..."), -VF(I, V, S, O), +LF_ORIGIN(I, V, S, O), seek_adj(S), seek_prep(S), seek_adj(O), seek_prep(V), seek_adv(V), build_pre(I, V, S, O), proc_logform()]
 
 # subject/object related adjective
 proc_logform() / (LF_ORIGIN(I, V, X, Y) & LF_ADJ(I, X, A) & PRE_LF(I, E, S, O)) >> [show_line(f"\nProc. adj. sub:",A), -LF_ADJ(I, X, A), -PRE_LF(I, E, S, O), join_adj_subj(I, E, S, O, A), proc_logform()]
